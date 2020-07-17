c941741ae534a6a065614f7f2de99d22f6cb7464
Maanav Singh <50304418+MaanavS16@users.noreply.github.com>
2020-07-17 16:15:37 -0400

    added bot.py

--- ss_discordBot/bot.py
+++ ss_discordBot/bot.py
@@ -6,8 +6,24 @@ load_dotenv('secrets.env')
 Token = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
-
+commandKey = '?Sea'
 
 @client.event
 async def on_ready():
     print("I'm alive papi")
+
+@client.event
+async def on_message(message):
+
+    #prevent self-response
+    if message.author == client.user:
+        return
+
+    if commandKey in message.content[0:len(commandKey)]:
+        responseEmote = '\N{THUMBS UP SIGN}'
+
+        await message.add_reaction(responseEmote)
+
+    await message.channel.send("Message")
+
+client.run(Token)
